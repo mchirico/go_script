0851894cd570af69473de3ad57076a37cadcf207
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-02-07 19:02:07 -0500

    More tests

--- yamlpkg/yamlpkg_test.go
+++ yamlpkg/yamlpkg_test.go
@@ -1,13 +1,80 @@
 package yamlpkg
 
 import (
+	"bytes"
+	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
 	code := m.Run()
 	os.Exit(code)
+}
+
+func TestReadError_File_Problem(t *testing.T) {
+
+	var str bytes.Buffer
+	log.SetOutput(&str)
+
+	c := Config{}
+	err := c.Read("///")
+	if err == nil {
+		t.FailNow()
+	}
+	if strings.Contains(str.String(),
+		"Error ioutil.ReadFile") != true {
+		t.Fatalf("Got:\n%s\n\nExpected:\n%s\n", str.String(),
+			"Error ioutil.ReadFile")
+	}
+
+}
+
+func TestReadError_Content_Problem(t *testing.T) {
+
+	file := "_junk.yaml"
+
+	d1 := []byte("a\n\n\b\n")
+	err := ioutil.WriteFile(file, d1, 0644)
+
+	var str bytes.Buffer
+	log.SetOutput(&str)
+
+	c := Config{}
+	err = c.Read(file)
+	if err == nil {
+		t.FailNow()
+	}
+	if strings.Contains(str.String(),
+		"Error Unmarshal") != true {
+		t.Fatalf("Got:\n%s\n\nExpected:\n%s\n", str.String(),
+			"Error Unmarshal")
+	}
+
+}
+
+func TestWriteError_File_Problem(t *testing.T) {
+
+	file := "///"
+	var str bytes.Buffer
+	log.SetOutput(&str)
+
+	c := Config{}
+	err := c.Write(file)
+	if err == nil {
+		t.FailNow()
+	}
+
+	expected := `error in yaml write: open ///: is a directory`
+
+	if strings.Contains(str.String(),
+		expected) != true {
+		t.Fatalf("Got:\n%s\n\nExpected:\n%s\n", str.String(),
+			expected)
+	}
+
 }
 
 func TestReadWrite(t *testing.T) {
